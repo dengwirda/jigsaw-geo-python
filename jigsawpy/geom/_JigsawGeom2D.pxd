c0a0322f5ab5e0782e0cc72fcf3a0ff8d7e4d23b
J.R. Calzada <jreniel@gmail.com>
2019-06-08 15:33:57 -0400

    Implementation updates.
    _MeshBoundaries is a base class that takes as input matplotlib.path.Path
    objects, and triggers calls to the parent object to initialize the
    jigsaw C structures.

--- jigsawpy/geom/_JigsawGeom2D.pxd
+++ jigsawpy/geom/_JigsawGeom2D.pxd
@@ -1,5 +1,9 @@
 #cython: language_level=3
-from jigsawpy.cjigsaw.jigsaw_jig_t cimport jigsaw_jig_t
+from jigsawpy.cjigsaw.jigsaw_msh_t cimport jigsaw_msh_t
+
 
 cdef class _JigsawGeom2D:
-	cdef jigsaw_jig_t __geom
+       
+    cdef jigsaw_msh_t __geom
+    cdef int _flags
+    cdef str __mesh_type
--- jigsawpy/geom/_JigsawGeom2D.pyx
+++ jigsawpy/geom/_JigsawGeom2D.pyx
@@ -1,7 +1,7 @@
 #cython: language_level=3
-from jigsawpy.cjigsaw.lib_jigsaw cimport jigsaw_init_jig_t
-from jigsawpy.cjigsaw.jigsaw_jig_t cimport jigsaw_jig_t
-from jigsawpy.cjigsaw.jigsaw_msh_t cimport jigsaw_VERT2_t, \
+from jigsawpy.cjigsaw.lib_jigsaw cimport jigsaw_init_msh_t
+from jigsawpy.cjigsaw.jigsaw_msh_t cimport jigsaw_msh_t, \
+                                           jigsaw_VERT2_t, \
                                            jigsaw_EDGE2_t, \
                                            jigsaw_BOUND_t
 from jigsawpy.cjigsaw.jigsaw_const cimport JIGSAW_EUCLIDEAN_MESH, \
@@ -10,49 +10,142 @@ from jigsawpy.cjigsaw.jigsaw_const cimport JIGSAW_EUCLIDEAN_MESH, \
                                            JIGSAW_ELLIPSOID_MESH, \
                                            JIGSAW_ELLIPSOID_GRID, \
                                            JIGSAW_ELLIPSOID_DUAL
+from libc.stdlib cimport malloc, free
+import numpy as np
 
 cdef class _JigsawGeom2D:
 
-    __mesh_type = JIGSAW_EUCLIDEAN_MESH
-
     def __cinit__(self):
-        cdef jigsaw_jig_t _geom
-        jigsaw_init_jig_t(&_geom)
+        cdef jigsaw_msh_t _geom
+        jigsaw_init_msh_t(&_geom)
         self.__geom = _geom
 
-    def __call__(self):
-        self.__geom._flags = self._flags
-        return self.__geom
+    def __init__(self, str mesh_type):
+        self._mesh_type  = mesh_type
+        self._set_vert2()
+        self._set_edge2()
+        # self._set_bound()
 
-    def __set_vert2(self, jigsaw_VERT2_t vert2):
-        self.__geom._vert2._data = vert2
-        raise NotImplementedError('Must determine size before proceeding.')
-        self.__geom._vert2._size = None
+    def _set_vert2(self):
+        cdef int _malloc
+        _malloc = len(self.OuterRing.vertices)
+        for InnerRing in self.InnerRings:
+            _malloc += len(InnerRing.vertices)
+        for Subdomain in self.Subdomains:
+            _malloc += len(Subdomain.vertices)
+        cdef jigsaw_VERT2_t *_vert2 = <jigsaw_VERT2_t *> malloc(_malloc * sizeof(jigsaw_VERT2_t))
+        if not _vert2:
+            raise MemoryError()
+        cdef int i = 0
+        for xy, _ in self.OuterRing.iter_segments():
+            _vert2[i]._ppos = xy
+            _vert2[i]._itag = i
+            i += 1
+        for InnerRing in self.InnerRings:
+            for xy, _ in InnerRing.iter_segments():
+                _vert2[i]._ppos = xy
+                _vert2[i]._itag = i
+                i += 1
+        for Subdomain in self.Subdomains:
+            for xy, _ in Subdomain.iter_segments():
+                _vert2[i]._ppos = xy
+                _vert2[i]._itag = i
+                i += 1
+        self.__geom._vert2._data = _vert2
+        self.__geom._vert2._size = _malloc
 
-    def __set_edge2(self, jigsaw_EDGE2_t edge2):
-        self.__geom._edge2._data = edge2
-        raise NotImplementedError('Must determine size before proceeding.')
-        self.__geom._edge2._size = None
+    def _set_edge2(self):
+        cdef int _malloc
+        _malloc = len(self.OuterRing.vertices)
+        for InnerRing in self.InnerRings:
+            _malloc += len(InnerRing.vertices)
+        for Subdomain in self.Subdomains:
+            _malloc += len(Subdomain.vertices)
+        cdef jigsaw_EDGE2_t *_edge2 = <jigsaw_EDGE2_t *> malloc(_malloc * sizeof(jigsaw_EDGE2_t))
+        if not _edge2:
+            raise MemoryError()
+        cdef int i = 0
+        for xy, code in self.OuterRing.iter_segments():
+            if code != 79:
+                _node = [i, i+1]
+            else:
+                _node = [i, 0]
+            _edge2[i]._node = _node
+            _edge2[i]._itag = i
+            i += 1
+        for InnerRing in self.InnerRings:
+            _i = i
+            for xy, code in InnerRing.iter_segments():
+                if code != 79:
+                    _node = [i, i+1]
+                else:
+                    _node = [i, _i]
+                _edge2[i]._node = _node
+                _edge2[i]._itag = i
+                i += 1
+        for Subdomain in self.Subdomains:
+            for xy, code in Subdomain.iter_segments():
+                if code != 79:
+                    _node = [i, i+1]
+                else:
+                    _node = [i, 0]
+                _edge2[i]._node = _node
+                _edge2[i]._itag = i
+                i += 1
+        self.__geom._edge2._data = _edge2
+        self.__geom._edge2._size = _malloc
 
-    def __set_bound(self, jigsaw_BOUND_t bound):
-        self.__geom._bound._data = bound
-        raise NotImplementedError('Must determine size before proceeding.')
-        self.__geom._bound._size = None
+    def _set_bound(self):
+        raise NotImplementedError
+    #     cdef int _malloc = 0
+    #     for Subdomain in self.Subdomains:
+    #         _malloc += len(Subdomain.vertices)
+    #     cdef jigsaw_BOUND_t *_bound = <jigsaw_BOUND_t *> malloc(_malloc * sizeof(jigsaw_BOUND_t))
+    #     if not _bound:
+    #         raise MemoryError()
+    #     cdef int i = 0
+    #     for Subdomain in self.Subdomains:
+    #         for xy, _ in Subdomain.iter_segments():
+    #             _bound[i]._itag = _itag
+    #             _bound[i]._indx = _indx
+    #             _bound[i]._kind = _kind
+    #             i += 1
+    #     self.__geom._bound._data = _bound
+    #     self.__geom._bound._size = _malloc
+
+    def __dealloc__(self):
+        free(self.__geom._vert2._data)
+        free(self.__geom._edge2._data)
+        free(self.__geom._bound._data)
 
     @property
     def mesh_type(self):
-        return self.__mesh_type
-
-    @mesh_type.setter
-    def mesh_type(self, mesh_type):
-        assert isinstance(mesh_type, (JIGSAW_EUCLIDEAN_MESH,
-                                      JIGSAW_EUCLIDEAN_GRID,
-                                      JIGSAW_EUCLIDEAN_DUAL,
-                                      JIGSAW_ELLIPSOID_MESH,
-                                      JIGSAW_ELLIPSOID_GRID,
-                                      JIGSAW_ELLIPSOID_DUAL))
-        self.__mesh_type = mesh_type
+        return self._mesh_type
 
     @property
-    def _flags(self):
-        return self.mesh_type
+    def _mesh_type(self):
+        return self.__mesh_type
+
+    @_mesh_type.setter
+    def _mesh_type(self, str mesh_type):
+        if mesh_type == "JIGSAW_EUCLIDEAN_MESH":
+           self.__geom._flags = JIGSAW_EUCLIDEAN_MESH
+        elif mesh_type == "JIGSAW_EUCLIDEAN_GRID":
+            self.__geom._flags = JIGSAW_EUCLIDEAN_GRID
+        elif mesh_type == "JIGSAW_EUCLIDEAN_DUAL":
+            self.__geom._flags = JIGSAW_EUCLIDEAN_DUAL
+        elif mesh_type == "JIGSAW_ELLIPSOID_MESH":
+            self.__geom._flags = JIGSAW_ELLIPSOID_MESH
+        elif mesh_type == "JIGSAW_ELLIPSOID_GRID":
+            self.__geom._flags = JIGSAW_ELLIPSOID_GRID
+        elif mesh_type == "JIGSAW_ELLIPSOID_DUAL":
+            self.__geom._flags = JIGSAW_ELLIPSOID_DUAL
+        else:
+            TypeError('mesh_type argument must be one of '
+                                 + '{}'.format(["JIGSAW_EUCLIDEAN_MESH",
+                                                "JIGSAW_EUCLIDEAN_GRID",
+                                                "JIGSAW_EUCLIDEAN_DUAL",
+                                                "JIGSAW_ELLIPSOID_MESH",
+                                                "JIGSAW_ELLIPSOID_GRID",
+                                                "JIGSAW_ELLIPSOID_DUAL"]))
+        self.__mesh_type = mesh_type
